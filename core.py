4e396bf39e98a7f50f0e3a632f0480bb83c741d3
flags <flags@srsfckn.biz>
2012-07-21 23:38:28 -0400

    mod_mail, error handling

--- core.py
+++ core.py
@@ -1,32 +1,25 @@
+import logging
 import time
 
-def add_alarm(user,when,what='Ding ding! Alarm is done.'):
-	if not user.has_key('alarms'):
-		user['alarms'] = []
-	
-	user['alarms'].append({'when':when,'what':what})
-
-def parse(commands,callback,channel,user):
-	#if user['alert_channel']:
-	_channel = user['alert_channel']
-	
+def parse(commands,callback,channel,user):	
 	if 'info' in commands:
 		callback.msg(user['alert_channel'],'%s, Follow: %s' % (user['name'],str(user['follow'])))
 		callback.msg(user['alert_channel'],'%s, Alert channel: %s' % (user['name'],user['alert_channel']))
 	elif 'follow' in commands:
 		if user['follow']:
-			callback.msg(_channel,'%s, I will no longer follow you.' % (user['name']))
+			callback.msg(channel,'%s, I will no longer follow you.' % (user['name']))
 			user['follow'] = False
 		else:
-			callback.msg(_channel,'%s, I will follow you.' % (user['name']))
+			callback.msg(channel,'%s, I will follow you.' % (user['name']))
 			user['follow'] = True
-	elif commands[0] == 'alarm' and len(commands)>=3:
-		if commands[1] == 'set':
-			if len(commands)>3:
-				_what = ' '.join(commands[3:])
-				add_alarm(user,commands[2],what=_what)
-				callback.msg(_channel,'%s: Alarm set for %s (%s)' % (user['name'],commands[2],_what))
+	elif commands[0] == 'load' and len(commands)==2:
+		try:
+			if callback.has_module(commands[1]):
+				callback.msg(channel,'%s: Module \'%s\' already loaded' % (user['name'],commands[1]))
 			else:
-				add_alarm(user,commands[2])
-				callback.msg(_channel,'%s: Alarm set for %s' % (user['name'],commands[2]))
-			
+				exec('import %s as temp' % (commands[1]))
+				callback.add_module(commands[1],temp)
+				callback.msg(channel,'%s: Loaded module %s' % (user['name'],commands[1]))
+		except ImportError:
+			callback.msg(channel,'%s: Could not load module %s' % (user['name'],commands[1]))
+			logging.error('No module named \'%s\'' % commands[1])
---
+++ mod_mail.py
@@ -0,0 +1,32 @@
+import time
+
+def tick(user,callback):
+	_curr_time = time.strptime(time.ctime())
+	
+	if user.has_key('messages'):
+		pass
+
+def add_mail(user,who,message):
+	if not user.has_key('messages'):
+		user['messages'] = []
+	
+	user['messages'].append({'to':who,
+		'message':' '.join(message),
+		'when':time.strftime('%A, %B %d at %H:%M')})
+
+def parse(commands,callback,channel,user):
+	if commands[0] == 'mail' and len(commands)>=3:
+		add_mail(user,commands[1],commands[2:])
+		callback.msg(channel,'%s: Message sent to %s' % (user['name'],commands[1]))
+
+def on_user_join(user,channel,callback):
+	for _user in callback.get_users():
+		if _user.has_key('messages'):
+			for message in _user['messages']:
+				if message['to'] == user:
+					callback.msg(user,'Private message from %s sent %s: %s' %
+						(_user['name'],str(message['when']),str(message['message'])))
+					_user['messages'].remove(message)
+
+def on_user_part(user,channel,callback):
+	pass
