50a39f7f7ecd4f0d89c8b0edc222ce238baf3097
flags <flags@srsfckn.biz>
2012-07-23 04:18:36 -0400

    mod_stats

---
+++ mod_stats.py
@@ -0,0 +1,29 @@
+def tick(callback):
+	pass
+
+def user_tick(user,callback):
+	if not user.has_key('stats'):
+		user['stats'] = {'seconds_online':0,'lines':0,'joins':0,'parts':0}
+	
+	user['stats']['seconds_online']+=1
+
+def parse(commands,callback,channel,user):
+	user['stats']['lines']+=1
+	
+	if commands[0] == 'stats':
+		for key in user['stats']:
+			callback.msg(channel,'%s, %s: %s' %
+				(user['name'],str(key),user['stats'][key]))
+
+def on_user_join(user,channel,callback):
+	for _user in callback.get_users():
+		if _user['name']==user:
+			_user['stats']['joins']+=1
+			return 0
+
+def on_user_part(user,channel,callback):
+	for _user in callback.get_users():
+		if _user['name']==user:
+			_user['stats']['parts']+=1
+			return 0
+	
