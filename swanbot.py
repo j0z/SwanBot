a85815914083e98108c6a0436d83cb8e4d8bce9e
flags <flags@srsfckn.biz>
2012-08-02 23:20:24 -0400

    Highlight only

--- swanbot.py
+++ swanbot.py
@@ -474,6 +474,7 @@ class SwanBot(irc.IRCClient):
 	
 	def irc_RPL_WHOREPLY(self, *nargs):
 		"Receive WHO reply from server"
+		print nargs[1]
 		register_user(nargs[1][5],'')
 		
 		for module in self.modules:
