fb7f0decd8b87aee23d036543846bcae1a344389
flags <flags@srsfckn.biz>
2012-08-04 17:31:36 -0400

    mod_freebase

---
+++ mod_freebase.py
@@ -0,0 +1,30 @@
+import urllib
+import json
+import re
+
+__url__ = 'https://www.googleapis.com/freebase/v1/text/en/%search%'
+
+def get_info(query):
+	_result = json.loads(''.join(urllib.urlopen(__url__.replace('%search%',query.lower())
+		.replace(' ','_').replace('\n','')).readlines()))
+	
+	try:
+		return _result['result']
+	except:
+		return 'Search returned nothing.'
+
+def tick(callback):
+	pass
+
+def user_tick(user,callback):
+	pass
+
+def parse(commands,callback,channel,user):
+	if commands[0] == '.search' and len(commands)>=2:
+		callback.msg(channel,'Result: %s' % (get_info(' '.join(commands[1:]))),to=user['name'])
+
+def on_user_join(user,channel,callback):	
+	pass
+
+def on_user_part(user,channel,callback):
+	pass
