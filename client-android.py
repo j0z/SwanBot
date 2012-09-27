db1b7a69838de663c36130aff93a1f41b97d0f0e
flags <flags@srsfckn.biz>
2012-09-27 16:35:53 -0400

    Node outputs and Android tests.

--- client-android.py
+++ client-android.py
@@ -2,10 +2,11 @@ from client import Client
 import android
 
 droid = android.Android()
+HOST = '10.238.82.100'
 
-_results = Client('localhost').get({'param':'find_nodes',
+_results = Client(HOST).get({'param':'find_nodes',
                         'query':{'type':'tweet','public':True},
                         'apikey':'testkey'})['results']
 
-for node in Client('localhost').get({'param':'get_nodes','nodes':_results,'apikey':'testkey'}):
+for node in Client(HOST).get({'param':'get_nodes','nodes':_results,'apikey':'testkey'}):
 	droid.say('Tweet from %s. %s' % (node['from'],droid['text']))
