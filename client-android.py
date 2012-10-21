d2c470b2cf3c16f63f6331ad5b5c42303592bf12
flags <jetstarforever@gmail.com>
2012-10-21 16:16:21 -0400

    Bugfix

--- client-android.py
+++ client-android.py
@@ -4,9 +4,8 @@ import android
 droid = android.Android()
 HOST = '192.168.1.2'
 
-_results = Client(HOST).get({'param':'find_nodes',
-                        'query':{'type':'speech','public':True},
-                        'apikey':'testkey'})['results']
+_results = Client(HOST,'testkey').get({'param':'find_nodes',
+                        'query':{'type':'speech','public':True}})['results']
 
-for node in Client(HOST).get({'param':'get_nodes','nodes':_results,'apikey':'testkey'})['results']:
+for node in Client(HOST,'testkey').get({'param':'get_nodes','nodes':_results})['results']:
 	droid.ttsSpeak('%s' % (node['text']))
