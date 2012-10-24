de35081148a0c176c4b42c1b82c35a983271b587
flags <flags@srsfckn.biz>
2012-10-24 02:11:45 -0400

    Debug stuff...

--- client-android.py
+++ client-android.py
@@ -58,7 +58,7 @@ def get_time_asleep(droid):
 	_results = _results['results']
 	
 	node = Client(HOST,'testkey').get({'param':'get_nodes','nodes':_results})['results'][0]
-	_fell_asleep_time = datetime.strptime(str(node['created']),'%m/%d/%Y %H:%M:%S')
+	_fell_asleep_time = datetime.strptime(str(node['created']),'%c')
 	
 	return (datetime.now()-_fell_asleep_time).seconds
 
